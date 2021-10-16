6f4edfe0944ddd85290f4850c677f2ea31f676ef
gottsch <17928819+gottsch@users.noreply.github.com>
2021-10-15 20:25:31 -0400

    Update ScoreIt.java

--- src/main/java/com/someguyssoftware/scoreit/ScoreIt.java
+++ src/main/java/com/someguyssoftware/scoreit/ScoreIt.java
@@ -39,7 +39,7 @@ public class ScoreIt {
 	// constants
 	public static final String MODID = "scoreit";
 	public static final String NAME = "Score It";
-	protected static final String VERSION = "1.0.0";
+	protected static final String VERSION = "1.1.0";
 
 	public static ScoreIt instance;
 	
